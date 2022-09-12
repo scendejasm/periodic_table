b16d9e8157c10deb8429801fd5f5a8ae9b179037
scendejasm <scendejasm@gmail.com>
2022-09-12 15:35:31 +0000

    feat: complete script

--- element.sh
+++ element.sh
@@ -15,15 +15,19 @@ else
     then 
       ATOMIC_NUMBER="$($PSQL "SELECT atomic_number FROM elements WHERE symbol='$1';")"
     else
-      ATOMIC_NUMBER="$(PSQL "SELECT atomic_number FROM elements WHERE name='$1';")"
+      ATOMIC_NUMBER="$($PSQL "SELECT atomic_number FROM elements WHERE name='$1';")"
     fi
   fi
   if [[ -z $ATOMIC_NUMBER ]]
   then
   # print "I could not find that element in the database"
-    echo -e "\nSorry, we couldn't find that element in the database.\n"
+    echo -e "I could not find that element in the database."
   else
-    echo -e "\nAtomic number is $ATOMIC_NUMBER\n"
+    SELECTED_ELEMENT=$($PSQL "SELECT e.atomic_number, e.name, e.symbol, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius, t.type FROM elements e FULL JOIN properties p ON e.atomic_number=p.atomic_number FULL JOIN types t ON t.type_id=p.type_id WHERE e.atomic_number=$ATOMIC_NUMBER;")
+    echo $SELECTED_ELEMENT | while read ATOMIC_NUMBER BAR NAME BAR SYMBOL BAR MASS BAR MELTING_POINT BAR BOILING_POINT BAR TYPE
+    do
+      echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+    done
   fi
   # if atomic number not found
 fi
